0156435f0c3386dd35f00b8b4467ce8c31ffa516
Hitalo <hitalofrota@gmail.com>
2024-05-28 21:52:59 -0300

    feature/create mokDatabases

---
+++ src/dataBase-mok/proteinDataBase.js
@@ -0,0 +1,20 @@
+const brothDataBase = [
+    {
+        id: '1',
+        imageInactive: 'path/to/imageInactive1.jpg',
+        imageActive: 'path/to/imageActive1.jpg',
+        name: 'Proteina 1',
+        description: 'Description for Broth 1',
+        price: 10.0
+    },
+    {
+        id: '2',
+        imageInactive: 'path/to/imageInactive2.jpg',
+        imageActive: 'path/to/imageActive2.jpg',
+        name: 'Proteina 2',
+        description: 'Description for Broth 2',
+        price: 15.0
+    }
+];
+
+export default brothDataBase;
