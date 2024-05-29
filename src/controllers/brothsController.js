c44bd4db6dd1ba5a20251a28723e1a0080168c6e
Hitalo <hitalofrota@gmail.com>
2024-05-28 22:17:44 -0300

    fix:name broths

---
+++ src/controllers/brothsController.js
@@ -0,0 +1,14 @@
+import BrothRepository from "../repositories/brothRepository";
+
+class BrothController {
+    static findAllBroths = BrothRepository.getAllbrothDataBase;
+    static findOneBroth = BrothRepository.getBrothById;
+    //OBS:
+    //metodos abaixo nao utilizados pela regra de negocio do projeto
+    //foi inserito ao projeto para caso, em um futuro, houver alguma implementacao
+    static createBroth = BrothRepository.addBroth;
+    static updateBroth = BrothRepository.updateBroth;
+    static deleteBroth = BrothRepository.deleteBroth;
+}
+
+export default BrothController;
