3092671483ff07ee4fe202b355978f97fe2d7a98
Hitalo <hitalofrota@gmail.com>
2024-06-11 19:58:04 -0300

    feature/include more images

--- src/dataBase-mok/brothDataBase.js
+++ src/dataBase-mok/brothDataBase.js
@@ -1,19 +1,27 @@
 const broths = [
     {
         id: '1',
-        imageInactive: 'path/to/imageInactive1.jpg',
-        imageActive: 'path/to/imageActive1.jpg',
-        name: 'Broth 1',
-        description: 'Description for Broth 1',
+        imageInactive: `https://drive.google.com/file/d/1SJEBHOcP3z3Zc3qhA9lO8H2Wo99IXUm6/view?usp=sharing`,
+        imageActive: `https://drive.google.com/file/d/1KOCyEv5NCSNw_3hxJtJ_xpFeiKjgWPk5/view?usp=sharing`,
+        name: 'Sopa Low Carb',
+        description: 'Sopa detox que irá transformar o seu físico',
         price: 10.0
     },
     {
         id: '2',
-        imageInactive: 'path/to/imageInactive2.jpg',
-        imageActive: 'path/to/imageActive2.jpg',
-        name: 'Broth 2',
-        description: 'Description for Broth 2',
+        imageInactive: `https://drive.google.com/file/d/1SJEBHOcP3z3Zc3qhA9lO8H2Wo99IXUm6/view?usp=sharing`,
+        imageActive: `https://drive.google.com/file/d/1NVOS-40XA0p8bbnk93RhqOSbEyMnRh_g/view?usp=sharing`,
+        name: 'Sopa de Verdura',
+        description: 'Sopa de verdura que irá transformar o seu cérebro',
         price: 15.0
+    },
+    {
+        id: '3',
+        imageInactive: `https://drive.google.com/file/d/1SJEBHOcP3z3Zc3qhA9lO8H2Wo99IXUm6/view?usp=sharing`,
+        imageActive: `https://drive.google.com/file/d/1ThcbpoIiG59xLw-VD_wEdIJJ7QW4uSs2/view?usp=sharing`,
+        name: 'Sopa de Variedades',
+        description: 'Sopa de variedades que irá transformar o seu metabolismo',
+        price: 20.0
     }
 ];
 
