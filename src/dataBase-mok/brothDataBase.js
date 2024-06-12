e624fef017cdbbc98ecc04e00ec5197917fe7195
Hitalo <hitalofrota@gmail.com>
2024-06-12 16:40:32 -0300

    teste

--- src/dataBase-mok/brothDataBase.js
+++ src/dataBase-mok/brothDataBase.js
@@ -1,24 +1,24 @@
 const broths = [
     {
         id: '1',
-        imageInactive: `https://img.freepik.com/vetores-gratis/comida-saudavel-fresca_24908-56412.jpg?w=740&t=st=1718220858~exp=1718221458~hmac=4f9cae8a58b6ad9145a812ee7162eeb5d2f66c14de50f62bbab1aa2bf141855a`,
-        imageActive: `https://img.freepik.com/vetores-gratis/comida-saudavel-fresca_24908-56412.jpg?w=740&t=st=1718220858~exp=1718221458~hmac=4f9cae8a58b6ad9145a812ee7162eeb5d2f66c14de50f62bbab1aa2bf141855a`,
+        imageInactive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
+        imageActive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
         name: 'Sopa Low Carb',
         description: 'Sopa detox que irá transformar o seu físico',
         price: 10.0
     },
     {
         id: '2',
-        imageInactive: `https://img.freepik.com/vetores-gratis/comida-saudavel-fresca_24908-56412.jpg?w=740&t=st=1718220858~exp=1718221458~hmac=4f9cae8a58b6ad9145a812ee7162eeb5d2f66c14de50f62bbab1aa2bf141855a`,
-        imageActive: `https://img.freepik.com/vetores-gratis/comida-saudavel-fresca_24908-56412.jpg?w=740&t=st=1718220858~exp=1718221458~hmac=4f9cae8a58b6ad9145a812ee7162eeb5d2f66c14de50f62bbab1aa2bf141855a`,
+        imageInactive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
+        imageActive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
         name: 'Sopa de Verdura',
         description: 'Sopa de verdura que irá transformar o seu cérebro',
         price: 15.0
     },
     {
         id: '3',
-        imageInactive: `https://img.freepik.com/vetores-gratis/comida-saudavel-fresca_24908-56412.jpg?w=740&t=st=1718220858~exp=1718221458~hmac=4f9cae8a58b6ad9145a812ee7162eeb5d2f66c14de50f62bbab1aa2bf141855a`,
-        imageActive: `https://img.freepik.com/vetores-gratis/comida-saudavel-fresca_24908-56412.jpg?w=740&t=st=1718220858~exp=1718221458~hmac=4f9cae8a58b6ad9145a812ee7162eeb5d2f66c14de50f62bbab1aa2bf141855a`,
+        imageInactive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
+        imageActive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
         name: 'Sopa de Variedades',
         description: 'Sopa de variedades que irá transformar o seu metabolismo',
         price: 20.0
