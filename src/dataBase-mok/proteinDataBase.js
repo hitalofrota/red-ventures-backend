7e4e6c33a44e6770d579f56f6a57daeabb0b45b5
Hitalo <hitalofrota@gmail.com>
2024-06-12 16:13:23 -0300

    teste

--- src/dataBase-mok/proteinDataBase.js
+++ src/dataBase-mok/proteinDataBase.js
@@ -2,7 +2,7 @@ const proteins = [
     {
         id: '1',
         imageInactive: '',
-        imageActive: `https://www.svgrepo.com/show/48239/user-online.svg`,
+        imageActive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS06DkVDHMJ94NcKFiJSZeXF1w_IgYl0x1U6Q&s`,
         name: 'Proteina 1',
         description: 'Description for Broth 1',
         price: 10.0
