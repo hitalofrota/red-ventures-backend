f7dbc2c9f2ae0b6dfa0e03e52767f3db77cbc378
Hitalo <hitalofrota@gmail.com>
2024-06-11 23:56:59 -0300

    teste

--- src/repositories/orderRepository.js
+++ src/repositories/orderRepository.js
@@ -1,18 +1,64 @@
 import orders from "../dataBase-mok/orderDatabase.js";
-import proteins from "../dataBase-mok/proteinDataBase.js";
-import broths from "../dataBase-mok/brothDataBase.js";
 import OrderIdService from "../services/orderIdService.js";
+
+const broths = [
+    {
+        id: '1',
+        imageInactive: `https://drive.google.com/file/d/1SJEBHOcP3z3Zc3qhA9lO8H2Wo99IXUm6/view?usp=sharing`,
+        imageActive: `https://drive.google.com/file/d/1fyHfQOkMNrauHeiS8xyvkXD9EJQqvpUd/view?usp=sharing`,
+        name: 'Sopa Low Carb',
+        description: 'Sopa detox que irá transformar o seu físico',
+        price: 10.0
+    },
+    {
+        id: '2',
+        imageInactive: `https://drive.google.com/file/d/1SJEBHOcP3z3Zc3qhA9lO8H2Wo99IXUm6/view?usp=sharing`,
+        imageActive: `https://drive.google.com/file/d/1NVOS-40XA0p8bbnk93RhqOSbEyMnRh_g/view?usp=sharing`,
+        name: 'Sopa de Verdura',
+        description: 'Sopa de verdura que irá transformar o seu cérebro',
+        price: 15.0
+    },
+    {
+        id: '3',
+        imageInactive: `https://drive.google.com/file/d/1SJEBHOcP3z3Zc3qhA9lO8H2Wo99IXUm6/view?usp=sharing`,
+        imageActive: `https://drive.google.com/file/d/1ThcbpoIiG59xLw-VD_wEdIJJ7QW4uSs2/view?usp=sharing`,
+        name: 'Sopa de Variedades',
+        description: 'Sopa de variedades que irá transformar o seu metabolismo',
+        price: 20.0
+    }
+];
+
+const proteins = [
+    {
+        id: '1',
+        imageInactive: 'path/to/imageInactive1.jpg',
+        imageActive: 'path/to/imageActive1.jpg',
+        name: 'Proteina 1',
+        description: 'Description for Broth 1',
+        price: 10.0
+    },
+    {
+        id: '2',
+        imageInactive: 'path/to/imageInactive2.jpg',
+        imageActive: 'path/to/imageActive2.jpg',
+        name: 'Proteina 2',
+        description: 'Description for Broth 2',
+        price: 15.0
+    }
+];
+
 
 class OrderRepository {
     static async addOrder(req, res) {
-        const { proteinId= "1", brothId= "1"} = req.body;
+        const { proteinId, brothId } = req.body;
 
         if (!proteinId || !brothId) {
+            console.log('Request body:', req.body); // Adicione este log para depuração
             return res.status(400).json({ error: 'proteinId and brothId are required' });
         }
 
-        const protein = proteins;
-        const broth = broths;
+        const protein = proteins.find(prot => prot.id === proteinId);
+        const broth = broths.find(brot => brot.id === brothId);
 
         if (!protein) {
             return res.status(404).json({ error: 'Protein not found' });
