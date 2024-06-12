28753f6a2173298161eb5106651938659a097ac6
Hitalo <hitalofrota@gmail.com>
2024-06-12 20:09:36 -0300

    fix:route to swagger prod

--- swagger.js
+++ swagger.js
@@ -3,16 +3,26 @@ import swaggerUi from 'swagger-ui-express';
 import path from 'path';
 
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'Documentation for my API',
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API Documentation',
+        version: '1.0.0',
+        description: 'Documentation for my API',
+      },
+      servers: [
+        {
+          url: 'http://localhost:3001',
+          description: 'Servidor Local',
+        },
+        {
+          url: 'https://red-ventures-backend.vercel.app',
+          description: 'Servidor de Produção',
+        },
+      ],
     },
-  },
-  apis: [path.resolve('docs/swagger.yaml')], // Ajuste o caminho para o arquivo YAML
-};
+    apis: [path.resolve('docs/swagger.yaml')],
+  };
 
 const swaggerSpec = swaggerJsdoc(options);
 
