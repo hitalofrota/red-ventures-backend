d3f2757270a338de3dcdbdd16057fd82c26624f9
Hitalo <hitalofrota@gmail.com>
2024-06-13 03:30:25 -0300

    teste router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,16 @@ import express from "express";
 import broths from "./brothsRoutes.js";
 import proteins from "./proteinsRoutes.js";
 import OrderController from "../controllers/orderController.js";
-
 const router = express.Router();
 
 const routes = (app) => {
     app.use(express.json());
 
-    app.use('/broths', broths);
-    app.use('/proteins', proteins);
+    app.use(broths);
+    app.use(proteins);
 
     router.post('/order', OrderController.createOrder);
     app.use(router);
-};
+}
 
 export default routes;
