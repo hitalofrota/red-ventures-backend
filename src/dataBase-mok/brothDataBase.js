d7ba12d7bfd9ddfd44c8389878ae440e27ff41d0
Hitalo <hitalofrota@gmail.com>
2024-06-14 17:41:47 -0300

    feature/create new data

--- src/dataBase-mok/brothDataBase.js
+++ src/dataBase-mok/brothDataBase.js
@@ -1,28 +1,14 @@
-const broths = [
-    {
-        id: '1',
-        imageInactive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
-        imageActive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
-        name: 'Sopa Low Carb',
-        description: 'Sopa detox que irá transformar o seu físico',
-        price: 10.0
-    },
-    {
-        id: '2',
-        imageInactive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
-        imageActive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
-        name: 'Sopa de Verdura',
-        description: 'Sopa de verdura que irá transformar o seu cérebro',
-        price: 15.0
-    },
-    {
-        id: '3',
-        imageInactive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
-        imageActive: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS43-iA7blZz05VBoHjcPmVq2B1xj-XR6MQJCDymoqpySK0qGyD1zHODkhOP7PloFTQ96o&usqp=CAU`,
-        name: 'Sopa de Variedades',
-        description: 'Sopa de variedades que irá transformar o seu metabolismo',
-        price: 20.0
-    }
-];
+const broths = {
+    "broths": [
+        {
+            "id": "1",
+            "imageInactive": "https://tech.redventures.com.br/assets/android-icon-192x192-eR__0JOi.png",
+            "imageActive": "https://tech.redventures.com.br/assets/android-icon-192x192-eR__0JOi.png",
+            "name": "Sopa Low Carb",
+            "description": "Sopa detox que irá transformar o seu físico",
+            "price": 10.0
+        }
+    ]
+}
 
 export default broths;
